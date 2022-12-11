bf1140a043e94a8a072ab65c53a2760d9ab84b98
abbas <abbas.pooramini.80@gmail.com>
2022-12-12 00:16:46 +0330

    Connect to mysql

--- backend/OnlineTravel/OnlineTravel/settings.py
+++ backend/OnlineTravel/OnlineTravel/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'OnlineTravel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlin_travel_service',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
