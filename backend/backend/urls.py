333c52ba17480bfee06d6670242d52a25ad74ba6
Dodangeh <dodangeh.ar78@gmail.com>
2022-12-21 22:32:14 +0330

    autorization

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""OnlineTravel URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import authentication.urls
 from django.contrib import admin
-from django.urls import path
-from authentication import urls
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
 urlpatterns = [
-    path('auth/', authentication.urls.path),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
